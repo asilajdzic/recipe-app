cae4106bed5e785cafdac1792598824f194a2a12
asilajdzic <125396448+asilajdzic@users.noreply.github.com>
2023-05-13 02:40:06 +0200

    added add, save, show recipes

---
+++ server/src/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+import { userRouter } from './routes/users.js';
+import { recipesRouter } from './routes/recipes.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/auth', userRouter);
+app.use('/recipes', recipesRouter);
+
+const options = { writeConcern: { w: 'majority', wtimeout: 1000 } };
+mongoose
+	.connect(process.env.MONGO_URL, options)
+	.then(() => {
+		console.log('Connected to database');
+	})
+	.catch((error) => {
+		console.log('Error connecting to database', error);
+	});
+
+app.listen(3001, () => console.log('Server started!'));
